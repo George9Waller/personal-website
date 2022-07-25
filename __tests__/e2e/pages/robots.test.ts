f2d235cc13de8c8fbddf105ffac6a08be1f6da44
George Waller <george@dabapps.com>
2022-07-26 00:21:39 +0100

    prettier

--- __tests__/e2e/pages/robots.test.ts
+++ __tests__/e2e/pages/robots.test.ts
@@ -2,5 +2,5 @@ import { test, expect } from "@playwright/test";
 
 test("should serve a robots file", async ({ page }) => {
   await page.goto("/robots.txt");
-  await expect(await page.content()).toMatchSnapshot()
+  await expect(await page.content()).toMatchSnapshot();
 });
