faff984d432a8008b33cec108f0d40fc9c02e788
George Waller <george@dabapps.com>
2022-07-30 15:14:51 +0100

    update nextauth secret

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -49,7 +49,7 @@ const credentialsProvider = CredentialsProvider({
 });
 
 export const authOptions: NextAuthOptions = {
-  secret: process.env.SECRET,
+  secret: process.env.SECRET_KEY,
   adapter: PrismaAdapter(prisma),
   session: {
     strategy: "jwt",
